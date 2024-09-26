f92f98fb4be14210263387558e2c929f6e883466
AndresM9901 <afmarin635@soy.sena.edu.co>
2024-09-26 11:48:51 -0500

    add: cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routeUser = require('./controllers/user.controller');
 const routeBike = require('./controllers/bike.controller');
 const routeEvent = require('./controllers/event.controller');
@@ -14,6 +15,26 @@ app.use(logger('dev'));
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use('/api/users', routeUser);
 app.use('/api/bikes', routeBike);
